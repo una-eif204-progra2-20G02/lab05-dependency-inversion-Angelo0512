85e9e883f2228443a3083ee931dd6401450c680f
Angelo <murillocalvo342@gmail.com>
2020-08-28 09:18:50 -0600

    Clase Sender.h

---
+++ src/Sender.h
@@ -0,0 +1,15 @@
+//
+// Created by Angelo on 28/8/2020.
+//
+
+#ifndef MY_PROJECT_NAME_SENDER_H
+#define MY_PROJECT_NAME_SENDER_H
+
+#endif //MY_PROJECT_NAME_SENDER_H
+
+#include <string>
+
+class Sender{
+public:
+    virtual std::string sendPayment() const = 0;
+};
